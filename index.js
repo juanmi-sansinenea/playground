16059a2dcd37f6b71901e0d3ace01ee7510fd2b2
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2021-02-04 21:28:03 +0100

    dropdown includes links

--- index.js
+++ index.js
@@ -9,10 +9,27 @@ window.addEventListener("scroll", function () {
   }
 });
 
-// Click to go to buttons
+// Click button > action
 const explore = document.querySelector(".explore");
 
-explore.addEventListener("click", ()=> {
-    //window.alert("WTF");
-    window.location.href = "./buttons.html"
+explore.addEventListener("click", () => {
+  window.location.href = "./buttons.html";
 });
+
+// launch dropdown menu, remove when mouse leaves dropdown, and remove when mouse leaves menu item too
+const launchDropdown = document.querySelector(".launch-dropdown");
+const dropdownContent = document.querySelector(".dropdown-content");
+
+launchDropdown.addEventListener("mouseover", visibleDropdown);
+launchDropdown.addEventListener("mouseleave", inVisibleDropdown);
+dropdownContent.addEventListener("mouseover", visibleDropdown);
+dropdownContent.addEventListener("mouseleave", inVisibleDropdown);
+
+function visibleDropdown() {
+  dropdownContent.classList.remove("invisible");
+  dropdownContent.classList.add("visible");
+}
+function inVisibleDropdown() {
+  dropdownContent.classList.remove("visible");
+  dropdownContent.classList.add("invisible");
+}
