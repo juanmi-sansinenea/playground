296def154a52728c7000e1f44a5811e86fff75a4
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2021-02-07 12:50:35 +0100

    menu items in js array

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// Menu items
+const m0 = document.querySelector(".m0");
+const m1 = document.querySelector(".m1");
+const m2 = document.querySelector(".m2");
+const m3 = document.querySelector(".m3");
+
+const menuArr = ["Deshpar", "Ikxmöhe", "Uhlekoen", "Prataggana"];
+m0.textContent = menuArr[0];
+m1.textContent = menuArr[1];
+m2.textContent = menuArr[2];
+m3.textContent = menuArr[3];
+
+
+
 // HIDE / SHOW MENU
 const nav = document.querySelector("nav");
 
